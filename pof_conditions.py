917fc0d9e92df701dad1963013000ece9c379dfc
Roldan Gammad <rgammad@uci.edu>
2019-06-14 01:51:01 -0700

    added AddCondition and AddAction + basic argument implementation (untested)

--- pof_conditions.py
+++ pof_conditions.py
@@ -12,6 +12,7 @@ class Conditions():
         :param itemlevel: item level
         :input itemlevel: int 0-100
         """
+        return "ItemLevel    " + itemlevel + " " + operator
 
     def DropLevel(self, droplevel, operator=0):
         """Level the item starts dropping at
@@ -88,58 +89,4 @@ class Conditions():
         :input maptier: 1-17
         :param operator: comparison operators
         :input operator: 0-5
-        """
-
-# =================================================================================================
-# Actions
-# =================================================================================================
-class Actions():
-
-    def SetBorderColor(self, r, g, b, a=255):
-        """
-        Sets the border colour of the item box in RGB values from 0-255
-        with optional Alpha (opacity) value of 0-255
-        """
-
-    def SetTextColor(self, r, g, b, a=255):
-        """
-        Sets the text colour of the item box in RGB values from 0-255
-        with optional Alpha (opacity) value of 0-255
-        """
-
-    def SetBackgroundColor(self, r, g, b, a=255):
-        """
-        Sets the colour of the item box in RGB values from 0-255
-        with optional Alpha (opacity) value of 0-255
-        """
-
-    def SetFontSize(self, fontsize=32):
-        """Sets the font size of the item text."""
-
-    def PlayAlertSound(self, id, volume):
-        """Plays the specified Alert Sound with optional volume when dropped.
-
-        Only one sound can be played at a time.
-        """
-
-    def PlayAlertSoundPositional(self, id, volume):
-        """Plays the specified Alert Sound with optional volume when dropped.
-
-        Only one sound can be played at a time.
-        """
-
-    def CustomAlertSound(self, filepath):
-        """
-        Plays the specified custom sound when a specified item drops.
-        (almost all of the common file extensions should be supported)
-        """
-
-    def MinimapIcon(self, size, color, shape):
-        """Displays an icon on the minimap for specified items."""
-
-    def PlayEffect(self, color, temp):
-        """
-            Displays a coloured beam of light above an item highlighted by an item filter.
-            Use the Temp parameter to have a beam only appear as the item drops.
-            Otherwise, it will be permanently visible.
         """
--- pof_filterblock.py
+++ pof_filterblock.py
@@ -13,12 +13,62 @@ class FilterBlock():
     lstcond = []
     lstact = []
 
-    def __init__(self, conditions, actions):
-        lstcond = conditions
-        lstact = actions
+    def __init__(self):
+        """init"""
 
-    def Show(self, show=true):
+    def Create(self, show=True):
         """ Begining of the filter block
         :param show: Shows or Hides items based on rest of the block
         :input show: bool true to show false to hide
         """
+
+    def AddCondition(self, *args):
+        """Add Conditions to FilterBlock
+        :param args: condition arguments
+        :input args: condition name, condition inputs...
+        """
+        if args[0] == "ItemLevel":
+            return self.lstcond.append(conditions.ItemLevel(args[1], args[2]))
+        elif args[0] == "DropLevel":
+            return self.lstcond.append(conditions.DropLevel(args[1], args[2]))
+        elif args[0] == "Quality":
+            return self.lstcond.append(conditions.Quality(args[1], args[2]))
+        elif args[0] == "Rarity":
+            return self.lstcond.append(conditions.Rarity(args[1], args[2]))
+        elif args[0] == "ItemClass":
+            return self.lstcond.append(conditions.ItemClass(args[1]))
+        elif args[0] == "BaseType":
+            return self.lstcond.append(conditions.BaseType(args[1]))
+        elif args[0] == "SocketAmount":
+            return self.lstcond.append(conditions.SocketAmount(args[1], args[2]))
+        elif args[0] == "LinkedSocket":
+            return self.lstcond.append(conditions.LinkedSocket(args[1], args[2]))
+        elif args[0] == "SocketGroup":
+            return self.lstcond.append(conditions.SocketGroup(args[1]))
+        elif args[0] == "MapTier":
+            return self.lstcond.append(conditions.MapTier(args[1]))
+
+    def AddAction(self, *args):
+        """Add Actions to FilterBlock
+        :param args: action arguments
+        :input args: action name, argument inputs...
+        """
+        if args[0] == "BorderColor":
+            return self.lstact.append(actions.SetBorderColor(args[1], args[2], args[3], args[4]))
+        elif args[0] == "TextColor":
+            return self.lstact.append(actions.SetTextColor(args[1], args[2], args[3], args[4]))
+        elif args[0] == "BackgroundColor":
+            return self.lstact.append(actions.SetBackgroundColor(args[1], args[2],
+                                                                 args[3], args[4]))
+        elif args[0] == "FontSize":
+            return self.lstact.append(actions.SetFontSize(args[1]))
+        elif args[0] == "AlertSound":
+            return self.lstact.append(actions.PlayAlertSound(args[1], args[2]))
+        elif args[0] == "AlertSoundPositional":
+            return self.lstact.append(actions.PlayAlertSoundPositional(args[1], args[2]))
+        elif args[0] == "CustomAlertSound":
+            return self.lstact.append(actions.CustomAlertSound(args[1]))
+        elif args[0] == "MinimapIcon":
+            return self.lstact.append(actions.MinimapIcon(args[1], args[2], args[3]))
+        elif args[0] == "PlayEffect":
+            return self.lstact.append(actions.PlayEffect(args[1], args[2]))
